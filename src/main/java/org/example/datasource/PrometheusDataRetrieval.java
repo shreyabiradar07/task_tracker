9d336951f966eb115374911360a7ae1ef9d20021
Shreya <shbirada@redhat.com>
2024-01-09 16:54:42 +0530

    add methods to query workload_info and populate JSON object

--- src/main/java/org/example/datasource/PrometheusDataRetrieval.java
+++ src/main/java/org/example/datasource/PrometheusDataRetrieval.java
@@ -5,9 +5,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 import com.google.gson.*;
 import org.slf4j.Logger;
@@ -15,23 +13,39 @@ import org.slf4j.LoggerFactory;
 import org.example.datasource.ImportDataJsonObject;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.Map;
+
 
 public class PrometheusDataRetrieval {
     private static final Logger LOGGER = LoggerFactory.getLogger(PrometheusDataRetrieval.class);
+
     public PrometheusDataRetrieval(){
         System.out.println("Prometheus instance being invoked");
     }
     public List<ImportDataJsonObject> retrievePrometheusData() {
         String PROMETHEUS_URL = "http://localhost:9090/api/v1/query";
-        String PROMETHEUS_QUERY = "sum by (namespace) (kube_namespace_status_phase{phase=\"Active\"})";
+        String ACTIVE_NAMESPACES_QUERY = "sum by (namespace) (kube_namespace_status_phase{phase=\"Active\"})";
+        String WORKLOAD_INFO_QUERY = "sum by (namespace, workload, workload_type) (namespace_workload_pod:kube_pod_owner:relabel)";
+
         List<ImportDataJsonObject> importDataList = new ArrayList<>();
+        // Fetch Active Namespaces
+        List<String> activeNamespaces = fetchActiveNamespaces(PROMETHEUS_URL, ACTIVE_NAMESPACES_QUERY);
+
+        // Fetch Workload Info
+        Map<String, List<Map<String, String>>> namespaceWorkloadMap = fetchWorkloadInfo(PROMETHEUS_URL, WORKLOAD_INFO_QUERY);
+
+        ImportDataJsonObject importData =   createImportDataObject(activeNamespaces, namespaceWorkloadMap);
+        importDataList.add(importData);
+
+        return importDataList;
+    }
+    private List<String> fetchActiveNamespaces(String url, String query) {
+        List<String> activeNamespaces = new ArrayList<>();
 
         try {
-            String encodedQuery = URLEncoder.encode(PROMETHEUS_QUERY, "UTF-8");
-            URL url = new URL(PROMETHEUS_URL + "?query=" + encodedQuery);
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
+            URL apiUrl = new URL(url + "?query=" + encodedQuery);
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
             connection.setRequestMethod("GET");
             int responseCode = connection.getResponseCode();
 
@@ -45,25 +59,20 @@ public class PrometheusDataRetrieval {
                 }
                 reader.close();
 
-                // Parse the JSON response from Prometheus to extract namespaces
-                List<String> namespaces = parsePrometheusResponse(response.toString());
-
-                // Create a ImportDataJsonObject instance including each namespace
-                    ImportDataJsonObject importData = createImportDataObject(namespaces);
-                    importDataList.add(importData);
-
+                // Parse the JSON response to extract active namespaces
+                activeNamespaces = parseActiveNamespaces(response.toString());
             } else {
                 System.out.println("Error - HTTP Response Code: " + responseCode);
             }
 
             connection.disconnect();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return importDataList;
+        return activeNamespaces;
     }
-    private List<String> parsePrometheusResponse(String jsonResponse) {
+    private List<String> parseActiveNamespaces(String jsonResponse) {
         List<String> namespaces = new ArrayList<>();
 
         // Parse the JSON response and extract namespaces
@@ -99,33 +108,131 @@ public class PrometheusDataRetrieval {
 
         return namespaces;
     }
+    private Map<String, List<Map<String, String>>> fetchWorkloadInfo(String url, String query) {
+        Map<String, List<Map<String, String>>> namespaceWorkloadMap = new HashMap<>();
 
-    private ImportDataJsonObject createImportDataObject(List<String> namespaces) {
-        ImportDataJsonObject output = new ImportDataJsonObject();
-        output.setVersion("1.0");
+        try {
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
+            URL apiUrl = new URL(url + "?query=" + encodedQuery);
 
-        // Set cluster and group details as needed
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Parse the JSON response to extract workload info by namespaces
+                namespaceWorkloadMap = parseWorkloadInfo(response.toString());
+            } else {
+                System.out.println("Error - HTTP Response Code: " + responseCode);
+            }
+
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return namespaceWorkloadMap;
+    }
+
+    private Map<String, List<Map<String, String>>> parseWorkloadInfo(String jsonResponse) {
+        Map<String, List<Map<String, String>>> namespaceWorkloadMap = new HashMap<>();
+
+        // Parse the JSON response and extract namespaces
+        JsonObject jsonObject = JsonParser.parseString(jsonResponse).getAsJsonObject();
+
+        // Check if the response status is "success" and it contains data
+        if (jsonObject.has("status") && jsonObject.get("status").getAsString().equals("success")
+                && jsonObject.has("data") && jsonObject.get("data").isJsonObject()) {
+
+            JsonObject dataObject = jsonObject.getAsJsonObject("data");
+
+            // Check if the response data contains "result" as an array
+            if (dataObject.has("result") && dataObject.get("result").isJsonArray()) {
+                JsonArray resultArray = dataObject.getAsJsonArray("result");
+
+                // Iterate through the "result" array to extract namespaces
+                for (JsonElement result : resultArray) {
+                    JsonObject resultObject = result.getAsJsonObject();
+
+                    // Check if the result object contains the "metric" field with "namespace"
+                    if (resultObject.has("metric") && resultObject.get("metric").isJsonObject()) {
+                        JsonObject metricObject = resultObject.getAsJsonObject("metric");
+
+                        Map<String, String> workloadEntry = new HashMap<>();
+                        // Extract the namespace value and add it to the list
+                        if (metricObject.has("namespace")) {
+                            String namespace = metricObject.get("namespace").getAsString();
+                            String workload_name = metricObject.get("workload").getAsString();
+                            String workload_type = metricObject.get("workload_type").getAsString();
+                            workloadEntry.put(workload_name,workload_type);
+
+                            // Using compute to compute the list for the namespace and add the workload entry
+                            namespaceWorkloadMap.compute(namespace, (key, oldValue) -> {
+                                if (oldValue == null) {
+                                    List<Map<String, String>> newList = new ArrayList<>();
+                                    newList.add(workloadEntry);
+                                    return newList;
+                                } else {
+                                    oldValue.add(workloadEntry);
+                                    return oldValue;
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        return namespaceWorkloadMap;
+    }
+
+    private ImportDataJsonObject createImportDataObject(List<String> activeNamespaces, Map<String, List<Map<String, String>>> namespaceWorkloadMap) {
+        ImportDataJsonObject dataObject = new ImportDataJsonObject();
+        dataObject.setVersion("1.0");
+
         ImportDataJsonObject.ClusterGroup clusterGroup = new ImportDataJsonObject.ClusterGroup();
         clusterGroup.setClusterGroupName("prometheus");
 
         ImportDataJsonObject.Cluster cluster = new ImportDataJsonObject.Cluster();
         cluster.setClusterName("k8s-cluster");
 
-        // Create a list to store multiple Namespace objects
         List<ImportDataJsonObject.Namespace> namespaceList = new ArrayList<>();
 
-        // Set the fetched namespace
-        for (String fetchedNamespace : namespaces) {
+        for (String namespaceName : activeNamespaces) {
             ImportDataJsonObject.Namespace namespace = new ImportDataJsonObject.Namespace();
-            namespace.setNamespaceName(fetchedNamespace);
+            namespace.setNamespaceName(namespaceName);
+
+            List<Map<String, String>> workloadMaps = namespaceWorkloadMap.getOrDefault(namespaceName, Collections.emptyList());
+            List<ImportDataJsonObject.Workload> workloadList = new ArrayList<>();
+
+            System.out.println("workload maps = "+workloadMaps);
+            for (Map<String, String> workloadMap : workloadMaps) {
+                for (Map.Entry<String, String> entry : workloadMap.entrySet()) {
+                    ImportDataJsonObject.Workload workload = new ImportDataJsonObject.Workload();
+                    workload.setWorkloadName(entry.getKey());
+                    workload.setWorkloadType(entry.getValue());
+                    workloadList.add(workload);
+                }
+            }
+
+
+            namespace.setWorkloads(workloadList);
             namespaceList.add(namespace);
         }
 
         cluster.setNamespaces(namespaceList);
         clusterGroup.setCluster(cluster);
-        output.setClusterGroup(clusterGroup);
+        dataObject.setClusterGroup(clusterGroup);
 
-        return output;
+        return dataObject;
     }
 
 
